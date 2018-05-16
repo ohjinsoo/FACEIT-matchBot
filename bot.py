687b307a49825d100b6b27f3cfc16ab2e31b4707
jinsoo <oh.jinsoo98@yahoo.com>
2018-05-16 17:02:03 -0500

    will send msg when the game is done, and whether the players lost/won

--- bot.py
+++ bot.py
@@ -13,9 +13,10 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
+		print('Logged in as')
+		print(client.user.name)
+		print(client.user.id)
+		await getMatch.repeat(client)
 
 def as_player(d):
 	ret = Player()
@@ -24,24 +25,38 @@ def as_player(d):
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('.stats '):
-    	player_nick = message.content[7:]
-    	faceitLink = os.environ['FACEIT_URL'] + '/players?nickname=' + player_nick
-    	requestForJson = requests.get(faceitLink, headers={"Authorization": os.environ['FACEIT_KEY']})
-    	dictOfPlayer = requestForJson.json()
+	if message.content.startswith('!hello'):
+		testPlayers = ['jinsooerino', 'NotPAWEL']
+		testGameRoom = '134d7306-3c5b-4433-a1fa-ede77825b2aa'
 
-    	if json.dumps(dictOfPlayer)[2:8] == 'errors':
-    		await client.send_message(message.channel, 'No such player found.')
-    		return
+		faceitLink = os.environ['FACEIT_URL'] + '/matches/' + testGameRoom + '/stats'
+		requestForJson = requests.get(faceitLink, headers={"Authorization": os.environ['FACEIT_KEY']})
+		dictOfGame = requestForJson.json()
 
-    	my_dict = {}
-    	my_dict['nickname'] = dictOfPlayer.get('nickname')
-    	my_dict['skill_level'] = dictOfPlayer.get('games').get('csgo').get('skill_level')
-    	my_dict['faceit_elo'] = dictOfPlayer.get('games').get('csgo').get('faceit_elo')
-    	my_dict['faceit_url'] = dictOfPlayer.get('faceit_url')[0:23] + "en/" + dictOfPlayer.get('faceit_url')[30:]
+		embed = discord.Embed(title="team x vs. team y", description="-------------------------------------------------------------------------------------------------", color=0x00ff00)
+		embed.add_field(name="Fiel1", value="hi", inline=False)
+		embed.add_field(name="", value="-------------------------------------------------------------------------------------------------", inline=False)
+		await client.send_message(client.get_channel('445775038373691395'), embed=embed)
 
-    	player1 = Player(json.dumps(my_dict))
-    	await client.send_message(message.channel, 'Player: ' + player1.nickname + "\nLevel: " + str(player1.skill_level) + "\nElo: "
-    														  + str(player1.faceit_elo)  + "\nURL: " + player1.faceit_url)
+
+	if message.content.startswith('.stats '):
+		player_nick = message.content[7:]
+		faceitLink = os.environ['FACEIT_URL'] + '/players?nickname=' + player_nick
+		requestForJson = requests.get(faceitLink, headers={"Authorization": os.environ['FACEIT_KEY']})
+		dictOfPlayer = requestForJson.json()
+
+		if json.dumps(dictOfPlayer)[2:8] == 'errors':
+			await client.send_message(message.channel, 'No such player found.')
+			return
+
+		my_dict = {}
+		my_dict['nickname'] = dictOfPlayer.get('nickname')
+		my_dict['skill_level'] = dictOfPlayer.get('games').get('csgo').get('skill_level')
+		my_dict['faceit_elo'] = dictOfPlayer.get('games').get('csgo').get('faceit_elo')
+		my_dict['faceit_url'] = dictOfPlayer.get('faceit_url')[0:23] + "en/" + dictOfPlayer.get('faceit_url')[30:]
+
+		player1 = Player(json.dumps(my_dict))
+		await client.send_message(message.channel, 'Player: ' + player1.nickname + "\nLevel: " + str(player1.skill_level) + "\nElo: "
+																	+ str(player1.faceit_elo)  + "\nURL: " + player1.faceit_url)
 
 client.run(os.environ['BOT_TOKEN'])
--- getMatch.py
+++ getMatch.py
@@ -1,56 +1,93 @@
+import discord
+import asyncio
 import requests
 import time
 import json
 import os
 import threading
 
-def printMatches(playersInGame, gameIds):
-	for i in range(0, len(playersInGame)):
-		currGameId = gameIds[i]
-		currPlayers.append(playersInGame[i])
-		for j in range(i + 1, len(playersInGame)):
-			if currGameId == gameIds[j]:
-				currPlayers.append(playersInGame[j])
-				del gameIds[j]
-				del playersInGame[j]
-				j = j - 1
+async def createEmbed(dictOfGame, currPlayers):
+    winningFaction = dictOfGame.get('results').get('winner')
+    isFactionOne = False
+    factionOneList = dictOfGame.get('teams').get('faction1').get('roster_v1')
+    for i in range(0, len(factionOneList)):
+        factionOneMember = factionOneList[i].get('nickname')
+        if factionOneMember == currPlayers[0]:
+            isFactionOne = True
+            break
 
-		faceitLink = os.environ['FACEIT_URL'] + '/matches/' + currGameId + '/stats'
-		requestForJson = requests.get(faceitLink, headers={"Authorization": os.environ['FACEIT_KEY']})
-		dictOfGame = requestForJson.json()
-
-		client.send_message(message.channel, dictOfGame.get('rounds').get('round_stats').get('Score'))
-
-
-def searchForAllMatches(players):
-	playersInGame = []
-	gameIds = []
-
-	for i in range(0, len(players)):
-		player = players[i]
-		
-		faceitLink = os.environ['FACEIT_URL'] + '/players/' + player['user_id'] + '/history?game=csgo&from=' + str(rightBound) + '&offset=0&limit=1'
-		requestForJson = requests.get(faceitLink, headers={"Authorization": os.environ['FACEIT_KEY']})
-		dictOfGames = requestForJson.json()
-
-		if len(dictOfGames.get('items')) != 0:
-			gameId = dictOfGames.get('items')[0].get('match_id')
-			playersInGame.append(player['user_id'])
-			gameIds.append(gameId)
+    outcome = ''
+    message = ''
+    message += currPlayers[0]
+    for i in range(1, len(currPlayers)):
+        message += ', ' + currPlayers[i]
+    # WIN CONDITIONS: They are faction one and winning faction is faction1, or they are faction two and winning faction is faction2
+    if (isFactionOne and winningFaction == 'faction1') or (not isFactionOne and winningFaction == 'faction2'):
+        outcome = 'WINNERS'
+        message += ' won lul'
+    else:
+        outcome = "LOSERS"
+        message += ' suck ROFL'
 
 
-	printMatches(playersInGame, gameIds)
+    embed = discord.Embed(title=outcome, description=message, color=0x00ff00)
 
+    return embed
+
+
+async def printMatches(playersInGame, gameIds, client):
+    for i in range(0, len(playersInGame)):
+        currGameId = gameIds[i]
+        currPlayers = []
+        currPlayers.append(playersInGame[i])
+        for j in range(i + 1, len(playersInGame)):
+            if currGameId == gameIds[j]:
+                currPlayers.append(playersInGame[j])
+                del gameIds[j]
+                del playersInGame[j]
+                j = j - 1
+
+
+        faceitLink = os.environ['FACEIT_URL'] + '/matches/' + currGameId
+        requestForJson = requests.get(faceitLink, headers={"Authorization": os.environ['FACEIT_KEY']})
+        print(requestForJson.json())
+        if requestForJson.status_code == 404:
+            return
+
+        dictOfGame = requestForJson.json()
+        embed = await createEmbed(dictOfGame, currPlayers)
+        await client.send_message(discord.Object(id='445775038373691395'), embed=embed)
+
+    rightBound = int(time.time())
+    await asyncio.sleep(60)
+    await repeat(client)
+
+
+async def searchForAllMatches(players, client):
+    playersInGame = []
+    gameIds = []
+
+    for i in range(0, len(players)):
+        player = players[i]
+        faceitLink = os.environ['FACEIT_URL'] + '/players/' + player['user_id'] + '/history?game=csgo&from=' + str(rightBound) + '&offset=0&limit=1'
+        requestForJson = requests.get(faceitLink, headers={"Authorization": os.environ['FACEIT_KEY']})
+        dictOfGames = requestForJson.json()
+
+        if len(dictOfGames.get('items')) != 0:
+            gameId = dictOfGames.get('items')[0].get('match_id')
+            playersInGame.append(player['nickname'])
+            gameIds.append(gameId)
+
+    await printMatches(playersInGame, gameIds, client)
 
 faceitLink = os.environ['FACEIT_URL'] + '/teams/' + os.environ['TEAM_ID']
 requestForJson = requests.get(faceitLink, headers={"Authorization": os.environ['FACEIT_KEY']})
 dictOfPlayer = requestForJson.json()
 members = dictOfPlayer.get('members')
+rightBound = int(time.time()) - 10000
 
-rightBound = int(time.time())
+async def repeat(c):
+    client = c
+    print(rightBound)
+    await searchForAllMatches(members, client)
 
-def repeat():
-  threading.Timer(60.0, repeat).start()
-  searchForAllMatches(members)
-
-repeat()
