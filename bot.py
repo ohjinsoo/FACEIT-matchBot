dfd2407c14f07aec48a1af00b7adeb461bba9e9c
ohjinsoo <jinsooo2@illinois.edu>
2018-05-18 17:10:31 -0500

    added fancy embedded messages for .stats and a check for if the player in .stats has csgo

--- bot.py
+++ bot.py
@@ -26,11 +26,24 @@ def as_player(d):
 	ret.__dict__.update(d)
 	return ret
 
+
+async def createStatsEmbed(player1):
+
+	embed = discord.Embed(color=0xC9FFFF)
+	embed.add_field(name='Level', value=player1.skill_level, inline=True)
+	embed.add_field(name='ELO', value=player1.faceit_elo, inline=True)
+	embed.add_field(name='Headshot %', value=player1.headshotPercentage + ' %', inline=True)
+	embed.add_field(name='Matches Played', value=player1.matches, inline=True)
+	embed.add_field(name='Win %', value=player1.winRate + ' %', inline=True)
+	embed.add_field(name='AVG. K/D Ratio', value=player1.kdRatio, inline=True)
+	embed.set_author(name=player1.nickname, icon_url=player1.avatar, url=player1.faceit_url)
+	return embed
+
 @client.event
 async def on_message(message):
 	if message.content.startswith('.stats '):
 		player_nick = message.content[7:]
-		faceitLink = os.environ['FACEIT_URL'] + '/players?nickname=' + player_nick
+		faceitLink = os.environ['FACEIT_URL'] + '/players?nickname=' + player_nick + '&game=csgo'
 
 		headers={"Authorization": os.environ['FACEIT_KEY']}
 		async with aiohttp.ClientSession(headers=headers) as session:
@@ -42,21 +55,39 @@ async def on_message(message):
 				# No permission to post or user does not exist.
 
 				if requestForJson.status != 200:
-					await client.send_message(message.channel, 'No such player found.')
+					await client.send_message(message.channel, 'No such player found. (stats is case sensitive)')
+					return
+
+				if 'csgo' not in dictOfPlayer.get('games'):
+					await client.send_message(message.channel, player_nick + ' has no stats for CSGO.')
 					return
 
 				# Only grab nickname, level, elo, and url as that is the only relevant variables.
 
-				my_dict = {}
-				my_dict['nickname'] = dictOfPlayer.get('nickname')
-				my_dict['skill_level'] = dictOfPlayer.get('games').get('csgo').get('skill_level')
-				my_dict['faceit_elo'] = dictOfPlayer.get('games').get('csgo').get('faceit_elo')
-				my_dict['faceit_url'] = dictOfPlayer.get('faceit_url')[0:23] + "en/" + dictOfPlayer.get('faceit_url')[30:]
+				player1 = Player()
+				player1.player_id = dictOfPlayer.get('player_id')
+				player1.nickname = dictOfPlayer.get('nickname')
+				player1.avatar = dictOfPlayer.get('avatar')
+				player1.skill_level = dictOfPlayer.get('games').get('csgo').get('skill_level')
+				player1.faceit_elo = dictOfPlayer.get('games').get('csgo').get('faceit_elo')
+				player1.faceit_url = dictOfPlayer.get('faceit_url')[0:23] + "en/" + dictOfPlayer.get('faceit_url')[30:]
 
-				# Create Player class using the dictionary.
+				faceitLink = os.environ['FACEIT_URL'] + '/players/' + player1.player_id + '/stats/csgo'
+				async with session.get(faceitLink) as requestForJson:
+					dictOfPlayerStats = await requestForJson.json()
+					
+					if requestForJson.status != 200:
+						await client.send_message(message.channel, player1.nickname + ' has no stats for CSGO.')
+						return
 
-				player1 = Player(json.dumps(my_dict))
-				await client.send_message(message.channel, 'Player: ' + player1.nickname + "\nLevel: " + str(player1.skill_level) + "\nElo: "
-																	+ str(player1.faceit_elo)  + "\nURL: " + player1.faceit_url)
+					player1.headshotPercentage = dictOfPlayerStats.get('lifetime').get('Average Headshots %')
+					player1.matches = dictOfPlayerStats.get('lifetime').get('Matches')
+					player1.winRate = dictOfPlayerStats.get('lifetime').get('Win Rate %')
+					player1.kdRatio = dictOfPlayerStats.get('lifetime').get('Average K/D Ratio')
+
+					embed = await createStatsEmbed(player1)
+
+					await client.send_message(message.channel, embed=embed)
+
 
 client.run(os.environ['BOT_TOKEN'])
--- models/Player.py
+++ models/Player.py
@@ -1,6 +1,13 @@
 import json
 
 class Player:
-
-    def __init__(self, j):
-         self.__dict__ = json.loads(j)
+	player_id = ''
+	nickname = ''
+	avatar = ''
+	skill_level = 0
+	faceit_elo = 0
+	faceit_url = ''
+	headshotPercentage = 0
+	matches = 0
+	winRate = 0
+	kdRatio = 0
