3530d358eeab10290bd6e9822cb016e379c8feb3
Jung Oh <jung@jungoh.me>
2018-05-20 14:41:34 -0700

    moved stats command

--- models/Player.py
+++ models/Player.py
@@ -1,13 +1,13 @@
 import json
 
 class Player:
-	player_id = ''
-	nickname = ''
-	avatar = ''
-	skill_level = 0
-	faceit_elo = 0
-	faceit_url = ''
-	headshotPercentage = 0
-	matches = 0
-	winRate = 0
-	kdRatio = 0
+    player_id = ''
+    nickname = ''
+    avatar = ''
+    skill_level = 0
+    faceit_elo = 0
+    faceit_url = ''
+    headshotPercentage = 0
+    matches = 0
+    winRate = 0
+    kdRatio = 0
