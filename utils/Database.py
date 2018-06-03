de3644dc4a84815a76605db3037b3931330bee35
ohjinsoo <jinsooo2@illinois.edu>
2018-06-03 14:31:15 -0500

    fixed db for moon runes

--- utils/Database.py
+++ utils/Database.py
@@ -6,8 +6,13 @@ from utils.Logger import log
 from config import DB_HOST, DB_USER, DB_PW, DB_NAME
 
 class Database:
-  db = MySQLdb.connect(host=DB_HOST, user=DB_USER, passwd=DB_PW, db=DB_NAME)
+  db = MySQLdb.connect(host=DB_HOST, user=DB_USER, passwd=DB_PW, db=DB_NAME, use_unicode=True)
   cur = db.cursor()
+
+  def __init__(self):
+    self.cur.execute('SET NAMES utf8mb4')
+    self.cur.execute("SET CHARACTER SET utf8mb4")
+    self.cur.execute("SET character_set_connection=utf8mb4")
 
   def __del__(self):
     log("::: Closing MySQL connection...")
