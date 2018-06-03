34ae17407fd98362cb29608cba182dc481b91d65
ohjinsoo <jinsooo2@illinois.edu>
2018-06-03 00:49:42 -0500

    edited runbot.sh

--- runBot.sh
+++ runBot.sh
@@ -3,4 +3,4 @@
 PID_FILE=/tmp/faceit_bot.pid
 [ -f $PID_FILE ] && kill -9 $(cat $PID_FILE) && rm $PID_FILE
 echo "Starting FACEIT_BOT"
-node src/index.js > /log/faceit_bot.log 2>&1 & echo $! >>$PID_FILE
+node src/index.js > /var/log/faceit_bot.log 2>&1 & echo $! >>$PID_FILE
