77921557108fdf441d59ff07c378d2d5053f2a05
ohjinsoo <jinsooo2@illinois.edu>
2018-06-03 00:57:32 -0500

    i promise this is the last runbot.sh commit

--- runBot.sh
+++ runBot.sh
@@ -3,4 +3,4 @@
 PID_FILE=/tmp/faceit_bot.pid
 [ -f $PID_FILE ] && kill -9 $(cat $PID_FILE) && rm $PID_FILE
 echo "Starting FACEIT_BOT"
-node src/index.js > /var/log/faceit_bot.log 2>&1 & echo $! >>$PID_FILE
+pipenv run python3.6 ./bot.py > /var/log/faceit_bot.log 2>&1 & echo $! >>$PID_FILE
